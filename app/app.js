461f3fe933a16e337da08aa2d4b3b4dec6e05a25
middtown <aarontm9@gmail.com>
2018-06-28 18:29:31 -0600

    refactor... good to say this was too easy... moving on :)

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('registrar');
   const input = form.querySelector('input');
-  
+
   const mainDiv = document.querySelector('.main');
   const ul = document.getElementById('invitedList');
   
@@ -81,33 +81,38 @@ document.addEventListener('DOMContentLoaded', () => {
       const button = e.target;
       const li = button.parentNode;
       const ul = li.parentNode;
-      if (button.textContent === 'remove') {
-        ul.removeChild(li);
-      } else if (button.textContent === 'edit') { 
-        const span = li.firstElementChild;
-        const input = document.createElement('input');
-        input.type = 'text';
-        input.value = span.textContent;
-        li.insertBefore(input, span);
-        li.removeChild(span);
-        button.textContent = 'save';
-      } else if (button.textContent === 'save') { 
-        const input = li.firstElementChild;
-        const span = document.createElement('span');
-        span.textContent = input.value;
-        li.insertBefore(span, input);
-        li.removeChild(input);
-        button.textContent = 'edit';
+      const action = button.textContent;
+
+
+ 
+      const nameActions = {
+        remove: () => {ul.removeChild(li);} ,
+        edit: () => {
+          const span = li.firstElementChild;
+          const input = document.createElement('input');
+          input.type = 'text';
+          input.value = span.textContent;
+          li.insertBefore(input, span);
+          li.removeChild(span);
+          button.textContent = 'save';
+        },
+        save: () => {
+          const input = li.firstElementChild;
+          const span = document.createElement('span');
+          span.textContent = input.value;
+          li.insertBefore(span, input);
+          li.removeChild(input);
+          button.textContent = 'edit';
+        }
       }
+        //select and run action via button's name
+        nameActions[action]();
     }
   });  
 });  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
+// 1. save info
+// 2. validate user info when form submitted
+// 3. change text label from confirmed to 'going' when selected
+// 4. add not comming element... maybe with selection dropdown
+// 5. 
